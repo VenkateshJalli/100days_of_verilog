ad6e7c1ba9d3d7a50c2ecc594e5b36771d7fb683
VenkateshJalli <jallivenkatesh3453@gmail.com>
2025-02-27 11:29:56 +0530

    Update Testbench.v

--- Day-7/Testbench.v
+++ Day-7/Testbench.v
@@ -1,22 +1,22 @@
-module comparator_1_bit_tb;
-  input a;
-  input b;
-  input c;
-  output reg x;
-  output reg y;
-  comparator_1_bit DUT(x,y,a,b,c);
+module bit1_comparator_tb;
+  input aGTb;
+  input aEQb;
+  input aLTb;
+  output reg a;
+  output reg b;
+	bit1_comparator DUT(a,b,aGTb,aEQb,aLTb);
 initial begin
   $dumpfile("comp.vcd");
-  $dumpvars(0,comparator_1_bit_tb);
+  $dumpvars(0,bit1_comparator_tb);
 // Initialize Inputs 
-	x = 1'b0; y = 1'b0;
-  #25 $display("Inputs x = %b, y = %b Outputs a =%b, b = %b, c = %b", x, y, a, b, c);
-  x = 1'b0; y = 1'b1;
-  #25 $display("Inputs x = %b, y = %b Outputs a =%b, b = %b, c = %b", x, y, a, b, c);
-  x = 1'b1; y = 1'b0;
-  #25 $display("Inputs x = %b, y = %b Outputs a =%b, b = %b, c = %b", x, y, a, b, c);
-  x = 1'b1; y = 1'b1;
-  #25 $display("Inputs x = %b, y = %b Outputs a =%b, b = %b, c = %b", x, y, a, b, c);
+	a = 1'b0; b = 1'b0;
+	#25 $display("Inputs a = %b, b = %b Outputs aGTb =%b, aEQb = %b, aLTb = %b", a, b, aGTb, aEQb, aLTb);
+  	a = 1'b0; b = 1'b1;
+	#25 $display("Inputs a = %b, b = %b Outputs aGTb =%b, aEQb = %b, aLTb = %b", a, b, aGTb, aEQb, aLTb);
+	a = 1'b1; b = 1'b0;
+	#25 $display("Inputs a = %b, b = %b Outputs aGTb =%b, aEQb = %b, aLTb = %b", a, b, aGTb, aEQb, aLTb);
+	a = 1'b1; b = 1'b1;
+	#25 $display("Inputs a = %b, b = %b Outputs aGTb =%b, aEQb = %b, aLTb = %b", a, b, aGTb, aEQb, aLTb);
   #25 $finish;
 end 
 endmodule
