ca97b6f95be8ad270e08b1948deb63f654bf2d3a
VenkateshJalli <jallivenkatesh3453@gmail.com>
2025-02-27 11:18:02 +0530

    Create Desing.v

---
+++ Day-7/Desing.v
@@ -0,0 +1,15 @@
+module comparator_1_bit(
+    input x,
+    input y,
+    output a,  //x>y
+    output b,  //x=y
+    output c   //x<y
+    );
+  wire xn;
+  wire yn;
+assign xn = ~ x;
+assign yn = ~ y;
+assign a = x & yn;
+assign c = xn & y;
+assign b = ~ ( a | c );
+endmodule
