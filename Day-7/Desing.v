cd92334e248c57a93a7f3fda47323c3dfba7ced4
VenkateshJalli <jallivenkatesh3453@gmail.com>
2025-02-27 11:31:55 +0530

    Update Desing.v

--- Day-7/Desing.v
+++ Day-7/Desing.v
@@ -1,15 +1,15 @@
-module comparator_1_bit(
-    input x,
-    input y,
-    output a,  //x>y
-    output b,  //x=y
-    output c   //x<y
+module bit1_comparator(
+    input a,
+    input b,
+    output aGTb,  //x>y
+    output aEQb,  //x=y
+    output aLTb   //x<y
     );
-  wire xn;
-  wire yn;
-assign xn = ~ x;
-assign yn = ~ y;
-assign a = x & yn;
-assign c = xn & y;
-    assign b = ~ ( x | y );
+  wire an;
+  wire bn;
+assign an = ~ a;
+assign bn = ~ b;
+assign aGTb = a & bn;
+assign aLTb = an & b;
+    assign aEQb = ~ ( a | b );
 endmodule
