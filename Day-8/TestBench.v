49e6c01bd4b3f816cfbb06eb17c7abfc42f6f2b8
VenkateshJalli <jallivenkatesh3453@gmail.com>
2025-02-28 11:09:14 +0530

    Update TestBench.v

--- Day-8/TestBench.v
+++ Day-8/TestBench.v
@@ -6,18 +6,14 @@ module bit4_comparator_tb;
   wire LT;
   bit4_comparator DUT(a, b, GT, EQ, LT);
   initial begin
+    integer i;
     $dumpfile("Comparator.vcd");
     $dumpvars(0,bit4_comparator_tb);
-    #25 a = 4'b0011; b = 4'b1001;
+    for(i=0;i<14;i++)
+      begin
+      #25 a = $random;  b = $random;
     $display("Time = %0t Inputs a = %b, b = %b Output: GT = %b, EQ = %b, LT = %b",$time, a, b, GT, EQ, LT);
-    #25 a = 4'b1011; b = 4'b1111;
-    $display("Time = %0t Inputs a = %b, b = %b Output: GT = %b, EQ = %b, LT = %b",$time, a, b, GT, EQ, LT);
-    #25 a = 4'b0011; b = 4'b0011;
-    $display("Time = %0t Inputs a = %b, b = %b Output: GT = %b, EQ = %b, LT = %b",$time, a, b, GT, EQ, LT);
-    #25 a = 4'b0011; b = 4'b0001;
-    $display("Time = %0t Inputs a = %b, b = %b Output: GT = %b, EQ = %b, LT = %b",$time, a, b, GT, EQ, LT);
-    #25 a = 4'b1011; b = 4'b1001;
-    $display("Time = %0t Inputs a = %b, b = %b Output: GT = %b, EQ = %b, LT = %b",$time, a, b, GT, EQ, LT);
-    #100 $finish;
+      end
+      #500 $finish;
   end
 endmodule
