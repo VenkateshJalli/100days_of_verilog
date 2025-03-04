7ba5b82e224151e044acc6ef0d8c9d88fc278367
VenkateshJalli <jallivenkatesh3453@gmail.com>
2025-03-04 11:24:24 +0530

    Update Design.v

--- Day-11/Design.v
+++ Day-11/Design.v
@@ -15,14 +15,14 @@ module hamming_encoder (
 endmodule
 
 module hamming_decoder (
-  input [7:0] hamming_in,
+  input [7:1] hamming_in,
   output [3:0] data_out,
   output error_flag,
   output [2:0] error_location
 );
 
   wire p1, p2, p4;
-  wire s1, s2, s4;
+  wire s1, s2, s4;//parity check bits s1=^in^p1
 
   assign p1 = hamming_in[1];
   assign p2 = hamming_in[2];
@@ -38,18 +38,18 @@ module hamming_decoder (
   assign data_out[3] = hamming_in[7];
   assign data_out[2] = hamming_in[6];
   assign data_out[1] = hamming_in[5];
-  assign data_out[0] = hamming_in[3] ^ (error_flag & error_location[0]);
+  assign data_out[0] = hamming_in[3];
 
   /*always @(*) begin
     if(error_flag) begin
       case(error_location)
-        3'b001: hamming_in[0] = ~hamming_in[0];
-        3'b010: hamming_in[1] = ~hamming_in[1];
-        3'b011: hamming_in[2] = ~hamming_in[2];
-        3'b100: hamming_in[3] = ~hamming_in[3];
-        3'b101: hamming_in[4] = ~hamming_in[4];
-        3'b110: hamming_in[5] = ~hamming_in[5];
-        3'b111: hamming_in[6] = ~hamming_in[6];
+        3'b001: hamming_in[1] = ~hamming_in[1];
+        3'b010: hamming_in[2] = ~hamming_in[2];
+        3'b011: hamming_in[3] = ~hamming_in[3];
+        3'b100: hamming_in[4] = ~hamming_in[4];
+        3'b101: hamming_in[5] = ~hamming_in[5];
+        3'b110: hamming_in[6] = ~hamming_in[6];
+        3'b111: hamming_in[7] = ~hamming_in[7];
         default: ;
       endcase
     end
