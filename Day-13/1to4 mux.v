0090f5289c5e051c2a3323736832dc670ed46355
VenkateshJalli <jallivenkatesh3453@gmail.com>
2025-03-05 19:14:21 +0530

    Update 1to4 mux.v

--- Day-13/1to4 mux.v
+++ Day-13/1to4 mux.v
@@ -6,6 +6,13 @@ module demux_1to4 (
   output out2,out3
 );
 
+ //using 1to2 Demux
+  wire n0,n1;
+  demux_1to2 u0(.data_in(data_in),.select(select[0]),.out0(n0),.out1(n1));
+  demux_1to2 u1(.data_in(n0),.select(select[1]),.out0(out0),.out1(out1));
+  demux_1to2 u2(.data_in(n1),.select(select[1]),.out0(out2),.out1(out3));
+
+  //dataflow modelling
   assign out0 = data_in & ~select[1] & ~select[0];
   assign out1 = data_in & ~select[1] & select[0];
   assign out2 = data_in & select[1] & ~select[0];
