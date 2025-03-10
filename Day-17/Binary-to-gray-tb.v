ce1bae5bb69a65569a7dc90ca85582fc1e215378
VenkateshJalli <jallivenkatesh3453@gmail.com>
2025-03-10 16:20:31 +0530

    Create Binary-to-gray-tb.v

---
+++ Day-17/Binary-to-gray-tb.v
@@ -0,0 +1,14 @@
+// Code your design here
+module tb;
+  reg [3:0]bi;
+  wire [3:0]gr;
+  binary_to_gray DUT(bi,gr);
+  initial begin
+    $dumpfile("bi_to_gr.vcd");
+    $dumpvars(1,tb);
+    repeat(15) begin
+      bi = $random;
+      #25 $display("Time = %0t, Inputs Binary = %b, Outputs Gary = %b",$time,bi,gr);
+    end
+  end 
+endmodule
